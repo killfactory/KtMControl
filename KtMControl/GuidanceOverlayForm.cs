8397c4eb94215e28d890df33fac1243520f496d6
kf.sama <test@test.com>
2026-03-15 08:47:54 +0100

    Move the guidance overlay form to a separate class, add tray icon and context menu

---
+++ KtMControl/GuidanceOverlayForm.cs
@@ -0,0 +1,82 @@
+﻿namespace KtMControl;
+
+internal sealed class GuidanceOverlayForm : Form
+{
+    private Rectangle activeArea = Rectangle.Empty;
+
+    public bool IsGuidanceVisible => Visible;
+
+    public GuidanceOverlayForm()
+    {
+        FormBorderStyle = FormBorderStyle.None;
+        ShowInTaskbar = false;
+        StartPosition = FormStartPosition.Manual;
+        TopMost = true;
+        BackColor = Color.Magenta;
+        TransparencyKey = Color.Magenta;
+        DoubleBuffered = true;
+    }
+
+    public void ShowGuidance(Screen screen, Rectangle area)
+    {
+        Bounds = screen.Bounds;
+        activeArea = area;
+
+        if (!Visible)
+        {
+            Show();
+        }
+
+        BringToFront();
+        Invalidate();
+    }
+
+    public void HideGuidance()
+    {
+        activeArea = Rectangle.Empty;
+        Hide();
+    }
+
+    protected override bool ShowWithoutActivation => true;
+
+    protected override CreateParams CreateParams
+    {
+        get
+        {
+            const int WsExToolWindow = 0x00000080;
+            const int WsExNoActivate = 0x08000000;
+
+            var cp = base.CreateParams;
+            cp.ExStyle |= WsExToolWindow | WsExNoActivate;
+            return cp;
+        }
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+
+        if (activeArea == Rectangle.Empty)
+        {
+            return;
+        }
+
+        var localArea = new Rectangle(
+            activeArea.Left - Bounds.Left,
+            activeArea.Top - Bounds.Top,
+            activeArea.Width,
+            activeArea.Height);
+
+        var x1 = localArea.Left + localArea.Width / 3;
+        var x2 = localArea.Left + (2 * localArea.Width) / 3;
+        var y1 = localArea.Top + localArea.Height / 3;
+        var y2 = localArea.Top + (2 * localArea.Height) / 3;
+
+        using var pen = new Pen(Color.Red, 2);
+
+        e.Graphics.DrawLine(pen, x1, localArea.Top, x1, localArea.Bottom);
+        e.Graphics.DrawLine(pen, x2, localArea.Top, x2, localArea.Bottom);
+        e.Graphics.DrawLine(pen, localArea.Left, y1, localArea.Right, y1);
+        e.Graphics.DrawLine(pen, localArea.Left, y2, localArea.Right, y2);
+    }
+}
--- KtMControl/MouseControlForm.cs
+++ KtMControl/MouseControlForm.cs
@@ -7,6 +7,9 @@ public partial class MouseControlForm : Form
     private readonly Button moveMouseButton = new();
     private readonly GuidanceOverlayForm guidanceOverlayForm = new();
     private readonly System.Windows.Forms.Timer ctrlStateTimer = new();
+    private readonly NotifyIcon trayIcon = new();
+    private readonly ContextMenuStrip trayMenu = new();
+    private readonly ToolStripMenuItem exitMenuItem = new("Exit");
 
     private const int WmHotkey = 0x0312;
     private const uint ModAlt = 0x0001;
@@ -70,6 +73,7 @@ public partial class MouseControlForm : Form
         InitializeComponent();
         InitializeMoveMouseButton();
         InitializeCtrlStateTimer();
+        InitializeTrayIcon();
     }
 
     protected override void OnHandleCreated(EventArgs e)
@@ -87,7 +91,7 @@ public partial class MouseControlForm : Form
         RegisterNumpadHotkey(HotkeyIdNumpad9, VkNumpad9);
         RegisterNumpadHotkey(HotkeyIdNumpadMultiply, VkMultiply);
         RegisterNumpadHotkey(HotkeyIdNumpadDivide, VkDivide);
-    }
+    }    
 
     protected override void OnFormClosed(FormClosedEventArgs e)
     {
@@ -105,8 +109,13 @@ public partial class MouseControlForm : Form
         UnregisterHotKey(Handle, HotkeyIdNumpadMultiply);
         UnregisterHotKey(Handle, HotkeyIdNumpadDivide);
 
+        trayIcon.Visible = false;
+        exitMenuItem.Click -= ExitMenuItem_Click;
+        trayIcon.Dispose();
+        trayMenu.Dispose();
         ctrlStateTimer.Dispose();
         guidanceOverlayForm.Dispose();
+
         base.OnFormClosed(e);
     }
 
@@ -155,6 +164,17 @@ public partial class MouseControlForm : Form
         ctrlStateTimer.Tick += CtrlStateTimer_Tick;
     }
 
+    private void InitializeTrayIcon()
+    {
+        exitMenuItem.Click += ExitMenuItem_Click;
+        trayMenu.Items.Add(exitMenuItem);
+
+        trayIcon.Text = "KtMControl";
+        trayIcon.Icon = SystemIcons.Application;
+        trayIcon.ContextMenuStrip = trayMenu;
+        trayIcon.Visible = true;
+    }
+
     private void MoveMouseButton_Click(object? sender, EventArgs e)
     {
         HandleNavigation(5);
@@ -166,6 +186,11 @@ public partial class MouseControlForm : Form
         {
             ResetImmediatelyToInitialState();
         }
+    }
+
+    private void ExitMenuItem_Click(object? sender, EventArgs e)
+    {
+        Close();
     }
 
     private void RegisterNumpadHotkey(int hotkeyId, uint virtualKey)
@@ -429,86 +454,5 @@ public partial class MouseControlForm : Form
         public uint dwFlags;
         public uint time;
         public nuint dwExtraInfo;
-    }
-}
-
-internal sealed class GuidanceOverlayForm : Form
-{
-    private Rectangle activeArea = Rectangle.Empty;
-
-    public bool IsGuidanceVisible => Visible;
-
-    public GuidanceOverlayForm()
-    {
-        FormBorderStyle = FormBorderStyle.None;
-        ShowInTaskbar = false;
-        StartPosition = FormStartPosition.Manual;
-        TopMost = true;
-        BackColor = Color.Magenta;
-        TransparencyKey = Color.Magenta;
-        DoubleBuffered = true;
-    }
-
-    public void ShowGuidance(Screen screen, Rectangle area)
-    {
-        Bounds = screen.Bounds;
-        activeArea = area;
-
-        if (!Visible)
-        {
-            Show();
-        }
-
-        BringToFront();
-        Invalidate();
-    }
-
-    public void HideGuidance()
-    {
-        activeArea = Rectangle.Empty;
-        Hide();
-    }
-
-    protected override bool ShowWithoutActivation => true;
-
-    protected override CreateParams CreateParams
-    {
-        get
-        {
-            const int WsExToolWindow = 0x00000080;
-            const int WsExNoActivate = 0x08000000;
-
-            var cp = base.CreateParams;
-            cp.ExStyle |= WsExToolWindow | WsExNoActivate;
-            return cp;
-        }
-    }
-
-    protected override void OnPaint(PaintEventArgs e)
-    {
-        base.OnPaint(e);
-
-        if (activeArea == Rectangle.Empty)
-        {
-            return;
-        }
-
-        var localArea = new Rectangle(
-            activeArea.Left - Bounds.Left,
-            activeArea.Top - Bounds.Top,
-            activeArea.Width,
-            activeArea.Height);
-
-        var x1 = localArea.Left + localArea.Width / 3;
-        var x2 = localArea.Left + (2 * localArea.Width) / 3;
-        var y1 = localArea.Top + localArea.Height / 3;
-        var y2 = localArea.Top + (2 * localArea.Height) / 3;
-
-        using var pen = new Pen(Color.Red, 2);
-
-        e.Graphics.DrawLine(pen, x1, localArea.Top, x1, localArea.Bottom);
-        e.Graphics.DrawLine(pen, x2, localArea.Top, x2, localArea.Bottom);
-        e.Graphics.DrawLine(pen, localArea.Left, y1, localArea.Right, y1);
-        e.Graphics.DrawLine(pen, localArea.Left, y2, localArea.Right, y2);
     }
 }
