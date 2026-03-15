f965a1a7535f20171c058d206ee2a1f37b23fce7
kf.sama <test@test.com>
2026-03-15 07:37:22 +0100

    Add an overlay form to show the rectangle

--- KtMControl/MouseControlForm.cs
+++ KtMControl/MouseControlForm.cs
@@ -5,6 +5,7 @@ namespace KtMControl;
 public partial class MouseControlForm : Form
 {
     private readonly Button moveMouseButton = new();
+    private readonly ScreenOverlayForm screenOverlayForm = new();
 
     private const int HotkeyId = 1;
     private const int WmHotkey = 0x0312;
@@ -44,6 +45,7 @@ public partial class MouseControlForm : Form
     protected override void OnFormClosed(FormClosedEventArgs e)
     {
         UnregisterHotKey(Handle, HotkeyId);
+        screenOverlayForm.Dispose();
         base.OnFormClosed(e);
     }
 
@@ -51,7 +53,7 @@ public partial class MouseControlForm : Form
     {
         if (m.Msg == WmHotkey && m.WParam == HotkeyId)
         {
-            MoveMouseToFirstScreen50_50();
+            MoveMouseAndShowRectangle();
             return;
         }
 
@@ -70,12 +72,95 @@ public partial class MouseControlForm : Form
 
     private void MoveMouseButton_Click(object? sender, EventArgs e)
     {
-        MoveMouseToFirstScreen50_50();
+        MoveMouseAndShowRectangle();
     }
 
-    private static void MoveMouseToFirstScreen50_50()
+    private void MoveMouseAndShowRectangle()
     {
         var firstScreen = Screen.AllScreens[0];
         Cursor.Position = new Point(firstScreen.Bounds.Left + 50, firstScreen.Bounds.Top + 50);
+        screenOverlayForm.ShowRectangleOnScreen(firstScreen);
+    }
+}
+
+internal sealed class ScreenOverlayForm : Form
+{
+    private readonly System.Windows.Forms.Timer hideTimer = new();
+    private Rectangle rectangleToDraw = Rectangle.Empty;
+
+    public ScreenOverlayForm()
+    {
+        FormBorderStyle = FormBorderStyle.None;
+        ShowInTaskbar = false;
+        StartPosition = FormStartPosition.Manual;
+        TopMost = true;
+        BackColor = Color.Magenta;
+        TransparencyKey = Color.Magenta;
+
+        hideTimer.Interval = 1000;
+        hideTimer.Tick += HideTimer_Tick;
+    }
+
+    public void ShowRectangleOnScreen(Screen screen)
+    {
+        Bounds = screen.Bounds;
+        rectangleToDraw = new Rectangle(2, 2, 96, 96);
+
+        if (!Visible)
+        {
+            Show();
+        }
+
+        BringToFront();
+        Invalidate();
+
+        hideTimer.Stop();
+        hideTimer.Start();
+    }
+
+    protected override bool ShowWithoutActivation => true;
+
+    protected override CreateParams CreateParams
+    {
+        get
+        {
+            const int WsExToolWindow = 0x00000080;
+            const int WsExNoActivate = 0x08000000;
+
+            var cp = base.CreateParams;
+            cp.ExStyle |= WsExToolWindow | WsExNoActivate;
+            return cp;
+        }
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+
+        if (rectangleToDraw == Rectangle.Empty)
+        {
+            return;
+        }
+
+        using var pen = new Pen(Color.Red, 3);
+        e.Graphics.DrawRectangle(pen, rectangleToDraw);
+    }
+
+    private void HideTimer_Tick(object? sender, EventArgs e)
+    {
+        hideTimer.Stop();
+        rectangleToDraw = Rectangle.Empty;
+        Hide();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            hideTimer.Tick -= HideTimer_Tick;
+            hideTimer.Dispose();
+        }
+
+        base.Dispose(disposing);
     }
 }
